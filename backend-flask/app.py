dbeb6e107ba196050442b2763cd9f399552ecc3f
Musab Mehadi <mussawayus3@gmail.com>
2023-05-11 20:05:18 +0000

    testing awscodepipeline

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -123,7 +123,7 @@ cors = CORS(
 #     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 @app.route('/api/health-check')
 def health_check():
-  return {'success': True}, 200
+  return {'success': True,'ver':1}, 200
 # @app.route('/rollbar/test')
 # def rollbar_test():
 #     rollbar.report_message('Hello World!', 'warning')
